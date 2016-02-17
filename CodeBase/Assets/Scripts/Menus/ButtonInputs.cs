c010f4038e4a2aaca5a027d1d47e54072e1892cd
Gerard Vega <vega.gerard@yahoo.com>
2016-02-17 14:31:52 -0500

    Did MainMenu.unity functionality. Added more folders for Scenes.

---
+++ CodeBase/Assets/Scripts/Menus/ButtonInputs.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class ButtonInputs : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        //EditorSceneManager.OpenScene();
+    }
+
+    public void Options()
+    {
+        //EditorSceneManager.OpenScene();
+    }
+
+    public void Credits()
+    {
+        //EditorSceneManager.OpenScene();
+    }
+
+    public void ExitGame()
+    {
+        
+        Application.Quit();
+
+        //Application.CancelQuit();
+    }
+}
