1d26bf87a5af1e863ce0d6df3b114a96ce2c49ae
Brandon <bjsvochak@fullsail.edu>
2016-02-18 05:04:05 -0500

    Updates To Enemy Movement --Added Object Pool Script --Spawning Enemies behind the big ship for now --Enemies despawn after circling a bit --Not entirely satisfied with the circling movement, may give it a second pass if we end up having time

--- GameJam/Assets/EnemyMove.cs
+++ GameJam/Assets/EnemyMove.cs
@@ -3,50 +3,158 @@ using System.Collections;
 
 public class EnemyMove : MonoBehaviour {
 
+    // player reference for passby
+    public Transform player;
+
     // nodes
     public Transform[] nodes;
-    private int next;
+    private int nextPatrol;
 
     // physics
-    public float standardSpeed = 120.0f;
-    public float rotationSpeed = 2.0f;
+    public float speed = 1200.0f;
+    public float rotationWidth = 2.0f;
     private Rigidbody rb;
-    public float midpoint;
+
+    // flags
+    private bool isCircling;
 
     // settings
-    public float threshold = 1.0f;  // how far from waypoint must we get before it is considered reached
+    public float threshold = 5.0f;  // how far from waypoint must we get before it is considered reached
+    public float playerDistanceThreshold = 5.0f;
+    public float secondsCircling = 20.0f;
+    public float circlingYOffset = 200.0f; // how far above the player the enemies should be circling;
+    public Vector3 circlingScale;
+    public float keepBearingsWhileCircling = 1.0f;
+    
+
+    private PooledObject pooledObject;
+
+    private Quaternion randomRotation;
+    private Quaternion addedRotation;
+
+    private Vector3 flyAwayDirection;
+
+    private float keepBearingsTimer;
 
     public void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        pooledObject = GetComponent<PooledObject>();
+    }
+
+    public void Reset()
+    {
         transform.position = nodes[0].position;
-        next = 1;
+        nextPatrol = 1;
+        randomRotation = addedRotation = Quaternion.identity;
+        flyAwayDirection = Vector3.zero;
+        isCircling = false;
+    }
+
+    public void Start()
+    {
+        Reset();
     }
 
     void Update()
     {
-        Patrol();
+        // patrolling, flying to player, passing by, then flying away
+        if (flyAwayDirection != Vector3.zero)
+            FlyAway();
+        if (nextPatrol < nodes.Length)
+            Patrol();
+        else
+            MoveTowardsPlayer();
     }
 
     void Patrol()
     {
-        // need strategy for reaching end of waypoint
-        if (next >= nodes.Length)
-            return;
+        Vector3 direction = nodes[nextPatrol].position - transform.position;
 
-        Vector3 direction = nodes[next].position - transform.position;
-
+        // go to the next node if we are now within range
         if (direction.magnitude <= threshold)
         {
-            direction = nodes[++next].position - transform.position;
+
+            // if next node doesn't exist, start the first PassBy
+            if(++nextPatrol >= nodes.Length)
+            {
+                GetRandomRotation();
+                MoveTowardsPlayer();              
+                return;
+            }
+
+            // node exists, so continue on to another node
+            direction = nodes[nextPatrol].position - transform.position;            
         }
 
-        float speed = standardSpeed;       
+        MoveShip(direction);
+    }
 
-        direction.Normalize();
-        Quaternion rotation = Quaternion.LookRotation(direction);
+    void FlyAway()
+    {
+        MoveShip(flyAwayDirection);
+    }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 1.0f / rotationSpeed);
+    IEnumerator FlyAwayRoutine()
+    {
+        yield return new WaitForSeconds(secondsCircling);
+        Vector3 playerDir = player.transform.position - transform.position;
+
+        // set the fly away direction so a random Vector3 away from the player
+        do
+            flyAwayDirection = Random.insideUnitSphere;
+        while (Vector3.Dot(playerDir, flyAwayDirection) > 0);
+
+        addedRotation = Quaternion.identity;
+
+        yield return new WaitForSeconds(30.0f);
+
+        Reset();
+        pooledObject.ObjectPool.ReturnObject(gameObject);
+    }
+
+    void GetRandomRotation()
+    {
+        randomRotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        randomRotation *= Quaternion.Euler(Random.Range(0.0f, 360.0f), 0.0f, 0.0f);
+    }
+
+    void MoveTowardsPlayer()
+    {
+        Vector3 playerPos = player.position;
+        playerPos.y += circlingYOffset;
+
+        Vector3 direction = playerPos - transform.position;
+
+        float circlingMagnitude = Vector3.Scale(direction, circlingScale).magnitude;
+
+        keepBearingsTimer -= Time.deltaTime;
+        if (circlingMagnitude < playerDistanceThreshold) //threshold is distance you want circle strafing to start
+        {
+            if (!isCircling)
+            {
+                isCircling = true;
+                StartCoroutine(FlyAwayRoutine());
+            }
+            addedRotation = randomRotation;
+            keepBearingsTimer = keepBearingsWhileCircling;
+        }
+        else if (keepBearingsTimer <= 0.0f)
+        {
+            addedRotation = Quaternion.identity;
+        }
+
+        MoveShip(direction);
+    }
+
+    void MoveShip(Vector3 _direction)
+    {
+        _direction.Normalize();
+        Quaternion rotation = Quaternion.LookRotation(_direction);
+        rotation *= addedRotation;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime / rotationWidth);
         rb.velocity = transform.forward * speed * Time.deltaTime;
     }
+
 }
